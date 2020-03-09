156596aeade5bf9af94c2fcf7ea3c713a2377956
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-09 13:19:48 +0100

    Add connection with database

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,39 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        connectWithDB();
+
     }
+
+    private static void connectWithDB() {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:AllteregoDB");
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Users");
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int basketId = rs.getInt("BasketId");
+                System.out.println("name = " + name + "\nBasketId = " + basketId);
+
+            }
+
+            rs.close();
+            stmt.close();
+            c.close();
+
+        } catch (Exception e) {
+            System.err.print(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Imported data from database successfully");
+    }
+
+
 }
