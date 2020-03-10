893dc8b8c1b76ecb51f0e8dd046a25fc37293b4f
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-10 12:12:07 +0100

    Repair connection with DB

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,14 +8,15 @@ import java.sql.*;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     private static User user;
 
     public static void main( String[] args )
     {
         System.out.println();
-        connectWithDB();
+//        connectWithDB();
+        connect();
 
     }
 
@@ -25,17 +26,18 @@ public class App
 
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:AllteregoDB");
+            c = DriverManager.getConnection("jdbc:sqlite:/home/acer/PROGRAMMING/Java Repositories/onlineShop/AllteregoDB");
             c.setAutoCommit(false);
             stmt = c.createStatement();
-            String potentialUser = "qwert";
-            String query  = "INSERT INTO Users(Name, BasketId, UserRankId)" +  "VALUES ('Bill', '0', '1');";
-//            String query  = "INSERT INTO Users(Name, BasketId, UserRankId)" +  "VALUES (?, ?, ?);";
+
+//            String query  = "INSERT INTO Users(Name, BasketId, UserRankId)" +  "VALUES ('Bill', '0', '1');";
+            String query  = "INSERT INTO Users(Name, BasketId, UserRankId)" +  "VALUES (?, ?, ?);";
 
             PreparedStatement preparedStmt = c.prepareStatement(query);
-//            preparedStmt.setString();
+            preparedStmt.setString(1, "Bill");
+            preparedStmt.setString(2, "0");
+            preparedStmt.setString(3, "1");
             preparedStmt.execute();
-
 
             ResultSet rs = stmt.executeQuery("SELECT * FROM Users;");
 
@@ -55,11 +57,30 @@ public class App
 
         } catch (Exception e) {
             System.err.print(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
         }
         System.out.println("\nImported data from database successfully");
         System.out.println("User name = " + user.getName());
     }
 
+    public static void connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:/home/acer/PROGRAMMING/Java Repositories/onlineShop/AllteregoDB";
+            conn = DriverManager.getConnection(url);
 
+            String query  = "INSERT INTO Users(Name, BasketId, UserRankId)" +  "VALUES (?, ?, ?);";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, "Bill");
+            preparedStmt.setString(2, "0");
+            preparedStmt.setString(3, "1");
+            preparedStmt.execute();
+
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
