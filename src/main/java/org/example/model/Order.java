bb00daf7e44e459810490b15f08517fa76c13479
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-25 16:53:27 +0100

    Add TestMain + Order

--- src/main/java/org/example/model/Order.java
+++ src/main/java/org/example/model/Order.java
@@ -1,4 +1,58 @@
 package org.example.model;
 
+import java.time.LocalDate;
+
 public class Order {
+    private int id;
+    private int userId;
+    private String status;
+    private LocalDate date = LocalDate.now();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setStatusAsOrdered() {
+        this.status = "Ordered";
+    }
+
+    public void setStatusAsPaid() {
+        this.status = "Paid";
+    }
+
+    public void setStatusOnTheWay() {
+        this.status = "On the way";
+    }
+
+    public void setStatusAsDelivered() {
+        this.status = "Delivered";
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
