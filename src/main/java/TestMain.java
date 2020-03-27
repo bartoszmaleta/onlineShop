0083e40e5403fbec1045ebd5f744dd1130d565e6
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-27 04:22:10 +0100

    Add two enters

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -6,11 +6,8 @@ import java.io.FileNotFoundException;
 public class TestMain {
 
     public static void main(String[] args) throws FileNotFoundException {
-//        System.out.println(DataHandler.stringFromFile("src/main/resources/menuLogo.txt"));
 //
-//        String stringToPrint = DataHandler.stringFromFile("src/main/resources/menuLogo.txt");
-//
-                LoggingController logging = new LoggingController();
+        LoggingController logging = new LoggingController();
         logging.init();
     }
 }
